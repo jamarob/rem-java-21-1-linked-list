db452776e0ac781edf3c9474ad9aa16b4116e17e
Fabian Schmauder <fabian.schmauder@gmail.com>
2021-01-28 10:32:54 +0100

    Add remove item

--- src/main/java/de/neuefische/linkedlist/AnimalListItem.java
+++ src/main/java/de/neuefische/linkedlist/AnimalListItem.java
@@ -1,5 +1,7 @@
 package de.neuefische.linkedlist;
 
+import java.util.Objects;
+
 public class AnimalListItem {
 
     private Animal value;
@@ -14,12 +16,12 @@ public class AnimalListItem {
     }
 
     public void add(Animal animal) {
-        if(value == null){
+        if (value == null) {
             value = animal;
         }
-        if(next == null){
+        if (next == null) {
             next = new AnimalListItem(animal);
-        }else {
+        } else {
             next.add(animal);
         }
     }
@@ -28,12 +30,49 @@ public class AnimalListItem {
         return next;
     }
 
+    public void remove(Animal animal) {
+        while (Objects.equals(value, animal)) {
+            if (next != null) {
+                value = next.value;
+                next = next.next;
+            } else {
+                value = null;
+                next = null;
+            }
+        }
+
+
+        AnimalListItem current = this;
+
+        while (current.next != null) {
+            if (Objects.equals(current.next.value, animal)) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+    }
+
     @Override
     public String toString() {
-        if(next == null){
+        if (next == null) {
             return value.toString();
-        }else {
+        } else {
             return value.toString() + " -> " + next.toString();
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AnimalListItem that = (AnimalListItem) o;
+        return Objects.equals(value, that.value) &&
+                Objects.equals(next, that.next);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value, next);
     }
 }
--- src/test/java/de/neuefische/linkedlist/AnimalListItemTest.java
+++ src/test/java/de/neuefische/linkedlist/AnimalListItemTest.java
@@ -1,5 +1,6 @@
 package de.neuefische.linkedlist;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -47,4 +48,153 @@ class AnimalListItemTest {
         // Then
         assertEquals("Dog -> Cat -> Capibara", actual);
     }
+
+
+    @Test
+    @DisplayName("Dog -> Cat -> Capibara  | remove Cat | Dog -> Capibara ")
+    public void removeMiddleItem(){
+        // Given
+        AnimalListItem head = new AnimalListItem(new Animal("Dog"));
+        head.add(new Animal("Cat"));
+        head.add(new Animal("Capibara"));
+
+        //WHEN
+        head.remove(new Animal("Cat"));
+
+        //THEN
+        AnimalListItem expected = new AnimalListItem(new Animal("Dog"));
+        expected.add(new Animal("Capibara"));
+        assertEquals(expected, head);
+
+    }
+
+    @Test
+    @DisplayName("Dog -> Cat -> Capibara  | remove Capibara | Dog -> Cat ")
+    public void removeLastItem(){
+        // Given
+        AnimalListItem head = new AnimalListItem(new Animal("Dog"));
+        head.add(new Animal("Cat"));
+        head.add(new Animal("Capibara"));
+
+        //WHEN
+        head.remove(new Animal("Capibara"));
+
+        //THEN
+        AnimalListItem expected = new AnimalListItem(new Animal("Dog"));
+        expected.add(new Animal("Cat"));
+        assertEquals(expected, head);
+
+    }
+
+
+    @Test
+    @DisplayName("Dog -> Cat -> Capibara  | remove Dog |  Cat -> Capibara ")
+    public void removeFirstItem(){
+        // Given
+        AnimalListItem head = new AnimalListItem(new Animal("Dog"));
+        head.add(new Animal("Cat"));
+        head.add(new Animal("Capibara"));
+
+        //WHEN
+        head.remove(new Animal("Dog"));
+
+        //THEN
+        AnimalListItem expected = new AnimalListItem(new Animal("Cat"));
+        expected.add(new Animal("Capibara"));
+        assertEquals(expected, head);
+
+    }
+
+
+    @Test
+    @DisplayName("Dog -> Cat -> Dog  | remove Dog |  Cat  ")
+    public void removeFirstAndLastItem(){
+        // Given
+        AnimalListItem head = new AnimalListItem(new Animal("Dog"));
+        head.add(new Animal("Cat"));
+        head.add(new Animal("Dog"));
+
+        //WHEN
+        head.remove(new Animal("Dog"));
+
+        //THEN
+        AnimalListItem expected = new AnimalListItem(new Animal("Cat"));
+        assertEquals(expected, head);
+
+    }
+
+    @Test
+    @DisplayName("Dog -> Dog -> Cat  | remove Dog |  Cat  ")
+    public void removeFirstTwo(){
+        // Given
+        AnimalListItem head = new AnimalListItem(new Animal("Dog"));
+        head.add(new Animal("Dog"));
+        head.add(new Animal("Cat"));
+
+        //WHEN
+        head.remove(new Animal("Dog"));
+
+        //THEN
+        AnimalListItem expected = new AnimalListItem(new Animal("Cat"));
+        assertEquals(expected, head);
+
+    }
+
+    @Test
+    @DisplayName("Dog -> Cat -> Dog  | remove Bird |  Dog -> Cat -> Dog   ")
+    public void removeNoItem(){
+        // Given
+        AnimalListItem head = new AnimalListItem(new Animal("Dog"));
+        head.add(new Animal("Cat"));
+        head.add(new Animal("Dog"));
+
+        //WHEN
+        head.remove(new Animal("Bird"));
+
+        //THEN
+        AnimalListItem expected = new AnimalListItem(new Animal("Dog"));
+        expected.add(new Animal("Cat"));
+        expected.add(new Animal("Dog"));
+
+        assertEquals(expected, head);
+
+    }
+
+
+    @Test
+    @DisplayName("Dog   | remove Dog |  null  ")
+    public void removeAllItems(){
+        // Given
+        AnimalListItem head = new AnimalListItem(new Animal("Dog"));
+
+
+        //WHEN
+        head.remove(new Animal("Dog"));
+
+        //THEN
+        AnimalListItem expected = new AnimalListItem(null);
+
+        assertEquals(expected, head);
+
+    }
+
+    @Test
+    @DisplayName("Dog  -> Cat -> Dog   | remove null |  Dog  -> Cat -> Dog  ")
+    public void removeNoItemWhenNull(){
+        // Given
+        AnimalListItem head = new AnimalListItem(new Animal("Dog"));
+        head.add(new Animal("Cat"));
+        head.add(new Animal("Dog"));
+
+        //WHEN
+        head.remove(null);
+
+        //THEN
+        AnimalListItem expected = new AnimalListItem(new Animal("Dog"));
+        expected.add(new Animal("Cat"));
+        expected.add(new Animal("Dog"));
+        assertEquals(expected, head);
+
+    }
+
 }
